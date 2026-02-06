f7759f415d7d155fdd29b12cd17b257d7785bdfb
1account-demo <nishantcard123@gmail.com>
2026-02-06 22:25:20 +0530

    initial clean setup

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const repo = "Birthday_2026";
+
+const nextConfig = {
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+};
+
+export default nextConfig;
+
+
